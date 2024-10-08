e855652b2f82e6968fc976eb01cf27de5ec55100
Itiligent <94789708+itiligent@users.noreply.github.com>
2024-10-08 17:16:36 +1100

    Update usb nic fling

--- esxi8.ps1
+++ esxi8.ps1
@@ -26,9 +26,8 @@ $nvmeFling = "nvme-community-driver_1.0.1.0-3vmw.700.1.0.15843807-component-1890
 	# For Esxi800 builds: ESXi800-VMKUSB-NIC-FLING-64098182-component-21668107.zip
 	# For ESXI80U1 builds: ESXi80U1-VMKUSB-NIC-FLING-64098092-component-21669994.zip
 	# For Esxi80U2 builds: ESXi80U2-VMKUSB-NIC-FLING-67561870-component-22416446.zip
-	# For Esxi80U3 builds: ESXi80U3-VMKUSB-NIC-FLING-75399077-component-24052746.zip
 	# Before manually upgrading Esxi, Remove old fling, upgrade, then install new Fling
-$usbFling = "ESXi80U3-VMKUSB-NIC-FLING-75399077-component-24052746.zip"
+$usbFling = "ESXi803-VMKUSB-NIC-FLING-76444229-component-24179899.zip"
 
 # Define Ghetto VCB repo for latest release download via Github API
 $ghettoUrl = "https://api.github.com/repos/lamw/ghettoVCB/releases/latest"
