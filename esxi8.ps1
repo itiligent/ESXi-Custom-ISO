4e40d9fc74613a2dbe322ad937d2ec0e9fc6f3e4
itiligent <94789708+itiligent@users.noreply.github.com>
2025-04-13 11:57:40 +1000

    add manual depot links

--- esxi8.ps1
+++ esxi8.ps1
@@ -12,45 +12,47 @@ $baseESXiVer = "8"
 # or 
 # https://higherlogicdownload.s3.amazonaws.com/BROADCOM/092f2b51-ca4c-4dca-abc0-070f25ade760/UploadedImages/Flings_Content/filename.zip"
 
-# Define Fling archive source link
+# Define source links
 $flingUrl = "https://raw.githubusercontent.com/itiligent/ESXi-Custom-ISO/main/8-updates/" # Fling archive in case they disappear again
-
-# Define NVME Fling filename - commented out as this is deprecated now
-$nvmeFling = "nvme-community-driver_1.0.1.0-3vmw.700.1.0.15843807-component-18902434.zip"
-
-# Define USB NIC Fling Filename
-	# For Esxi800 builds: ESXi800-VMKUSB-NIC-FLING-64098182-component-21668107.zip
-	# For ESXI80U1 builds: ESXi80U1-VMKUSB-NIC-FLING-64098092-component-21669994.zip
-	# For Esxi80U2 builds: ESXi80U2-VMKUSB-NIC-FLING-67561870-component-22416446.zip
-	# Before manually upgrading Esxi, Remove old fling, upgrade, then install new Fling
 $usbFling = "ESXi803-VMKUSB-NIC-FLING-76444229-component-24179899.zip"
-
-# Removed ghetoVCB support until vibs are compatible with esxi 8. Manually add ghetto scripts to ESXi in meantime.
-	# Define Ghetto VCB repo for latest release download via Github API
-	#$ghettoUrl = "https://api.github.com/repos/lamw/ghettoVCB/releases/latest"
-	#$ghettoVCB = "vghetto-ghettoVCB-offline-bundle-8x.zip"
-
-	# Set up user agent to avoid GitHub API rate limiting issues
-	#$headers = @{
-	#    "User-Agent" = "PowerShell"
-	#} | Out-Null
-
-	# Fetch the latest release information from Ghetto VCB GitHub API
-	#$response = Invoke-RestMethod -Uri $ghettoUrl -Headers $headers
-
-	# Extract Ghetto VCB download URL for the specific asset
-	#$ghettoDownloadUrl = $response.assets | Where-Object { $_.name -eq $ghettoVCB } | Select-Object -ExpandProperty browser_download_url
-
-	# Ghetto download the file
-	#Invoke-WebRequest -Uri $ghettoDownloadUrl -OutFile $ghettoVCB
+$manualUpdate1 = "ESXi-8.0U3e-24674464-standard.zip"
+$manualUpdateUrl1 = "https://my.microsoftpersonalcontent.com/personal/d019e1a076a71cc7/_layouts/15/download.aspx?UniqueId=f5954d1d-748f-435d-826d-98ce8e98b7ab&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiI4ODVlNTNhZC1kMmJhLTQ0MTktYjdiZS1jYmRmMTg5MjQ1OTEiLCJhcHBpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDA0ODE3MTBhNCIsImF1ZCI6IjAwMDAwMDAzLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMC9teS5taWNyb3NvZnRwZXJzb25hbGNvbnRlbnQuY29tQDkxODgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsImV4cCI6IjE3NDQ1MTYyNDIifQ.hysKNEqutu2qoDGwPVcn-VACWYuzx--L_ru5uxcZifYEHsD4tINxB6umRO-7WvMS1NhcTqEcRtR601q6YgW_DOR-TlMzp2HeXPc7tpqRrG-fVO3z2PxQzR4B9FFluQEMPjOWccZkx5FQGWsJSaClpd8sf-sImwf7njIq7YcDYJATlfuFCZgUO88SCdjmEqzj2wqVZF6hV-OqXMQERhO9p2Bnhk99j30LjMfSJAwall3Qt2KYjVeH3kObhXaibSgaKXYPfgsmvrmkxDyzwuxJyf3d5eMEkrCLQ8136DCnayLbjoXdlXHiHYTWCMJstKwN3IXIsUQbAt-vWrnzlX8MxxiOlAOcHdGMEQHXTHfdCLLLEn55FFORJT6CR5Hnz3F-kHTAV65gSie16G_xB3ngsg.zfNP9bVAlqMK3NeJ9n_zu_XuSecUoa74g8p-o6HjYuM&ApiVersion=2.0"
+# Run this script in the same direcrtory as file $manualUpdate1 to build locally without downloading    
 
 echo ""
-echo "Retrieving ESXi $baseESXiVer installation bundles to choose from, this may take a while..."
+echo "Retrieving latest ESXi $baseESXiVer release information..."
 echo ""
 
-Add-EsxSoftwareDepot https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
-$imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
-echo ""
+# Prompt user for update choice
+do {
+    echo "Choose a specific release:"
+    echo "1. Manually downloaded depot $($manualUpdate1)"
+    echo "2. Choose an image profile from VMware's online index"
+    echo "" 
+   $choice = Read-Host "Enter your choice (1-2)"
+} while ($choice -notmatch "^[1-2]$")
+
+switch ($choice) {
+    "1" {
+        echo ""
+        echo "Downloading $($manualUpdate1) & creating ESXi depot"
+        if (!(Test-Path $manualUpdate1)){Invoke-WebRequest -Uri $manualUpdateUrl1 -OutFile $($manualUpdate1)}
+        Add-EsxSoftwareDepot $manualUpdate1
+        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
+    }
+    
+    
+    "2" {
+        echo ""
+        echo "Downloading vmw-depot-index.xml & building ESXi depot, please be patient..."
+        # Retrieve available image profiles from VMware
+        Add-EsxSoftwareDepot https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
+        echo ""
+        echo "ESXi-8.0U3e-24674464-standard was the last public download" 
+    }
+}
+
 # Print a list of available profiles to choose from
 for ($i = 0; $i -lt $imageProfiles.Count; $i++) {
     echo "$($i + 1). $($imageProfiles[$i].Name) - Created on: $($imageProfiles[$i].CreationTime)"
@@ -75,8 +77,6 @@ echo "Finished retrieving $imageProfile"
 echo ""
 
 if (!(Test-Path $usbFling)){Invoke-WebRequest -Method "GET" $flingUrl$($usbFling) -OutFile $($usbFling)}
-#if (!(Test-Path $nvmeFling)){Invoke-WebRequest -Method "GET" $flingUrl$($nvmeFling) -OutFile $($nvmeFling)}
-#if (!(Test-Path $ghettoVCB)){Invoke-WebRequest -Uri $ghettoDownloadUrl -OutFile $($ghettoVCB)}
 
 echo ""
 echo "Adding extra packages to the local depot"
@@ -84,8 +84,6 @@ echo ""
 
 Add-EsxSoftwareDepot "$($imageProfile).zip"
 Add-EsxSoftwareDepot $usbFling
-#Add-EsxSoftwareDepot $nvmeFling
-#Add-EsxSoftwareDepot $ghettoVCB
 
 echo ""
 echo "Creating a custom profile" 
@@ -100,8 +98,6 @@ echo "Injecting extra packages into the custom profile"
 echo ""
 
 Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "vmkusb-nic-fling" -Force
-#Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "nvme-community" -Force
-#Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "ghettoVCB" -Force
 
 echo ""
 echo "Exporting the custom profile to an ISO..."
