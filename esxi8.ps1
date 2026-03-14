e54446ad5f3dd31381af62fc2c654e1c2c4252e5
itiligent <94789708+itiligent@users.noreply.github.com>
2026-03-14 13:34:25 +1100

    Update esxi8.ps1

--- esxi8.ps1
+++ esxi8.ps1
@@ -1,61 +1,35 @@
 ##############################################################################################
 # Build custom ESXi 8.x ISOs for non HCL hardware
 # David Harrop
-# February 2024
+# March 2025
 ##############################################################################################
 
 # Set ESXi depot base version
 $baseESXiVer = "8"
-$TOKEN = ""
 
-# Dowload Flings from Broadcom here: 
+# Download other VMWare Flings from Broadcom here: 
 # https://support.broadcom.com/group/ecx/productdownloads?subfamily=Flings&freeDownloads=true
 
-# Define Fling source file & link
-$flingUrl = "https://raw.githubusercontent.com/itiligent/ESXi-Custom-ISO/main/8-updates/" # Fling archive in case they disappear again
-$usbFling = "ESXi803-VMKUSB-NIC-FLING-76444229-component-24179899.zip"
+# Define Fling file names & download links (Adapt these with the versions you wish to use)
+$flingUrl = "https://raw.githubusercontent.com/itiligent/ESXi-Custom-ISO/main/8-updates/" # Flings archived here in case they disappear again
+$usbNicFling = "ESXi803-VMKUSB-NIC-FLING-93415869-component.zip"
+$realtekNicFling = "VMware-Re-Driver_1.101.01-5vmw.800.1.0.20613240.zip"
 
-# Nominate a custom esxi depot zip file and link:
-# (Run this script in the same direcrtory as file $manualUpdate1 to build locally without downloading)
-#$manualUpdate1 = "ESXi-8.0U3e-24674464-standard.zip"
-#$manualUpdateUrl1 = "https://itiligent-my.sharepoint.com/personal/david_itiligent_com_au/_layouts/15/guestaccess.aspx?share=Ed1VKVshlPNGu4sRc22DGmsBm1eDtdfP-PuqXB8AErs7yg&download=1" #VMware-ESXi-8.0U3e-24674464-depot.zip
+# Define the esxi depot zip file name & download link (Adapt these with the versions you wish to use)
+# (Download frpm links, or run this script from the same direcrtory as the local source files)
 $manualUpdate1 = "VMware-ESXi-8.0U3g-24859861-depot.zip"
 $manualUpdateUrl1 = "https://itiligent-my.sharepoint.com/personal/david_itiligent_com_au/_layouts/15/guestaccess.aspx?share=EW_xj8c5HTFLmoOeiNsjwKgBtIhKez8HmDNQKwoH6I5etg&e=udgNQt&download=1"
 
-echo ""
-echo "Retrieving latest ESXi $baseESXiVer release information..."
-echo ""
+Write-Host ""
+Write-Host "Preparing local ESXi depot and package files..."
+Write-Host ""
 
-# Prompt user for update choice
-do {
-    echo "Choose a specific release:"
-    echo "1. Manually downloaded depot $($manualUpdate1)"
-    echo "2. Choose image profile from VMware online index - REQUIRES BROADCOM TOKEN"
-    echo "" 
-   $choice = Read-Host "Enter your choice (1-2)"
-} while ($choice -notmatch "^[1-2]$")
-
-switch ($choice) {
-    "1" {
-        echo ""
-        echo "Downloading $($manualUpdate1) & creating ESXi depot"
-        if (!(Test-Path $manualUpdate1)){Invoke-WebRequest -Uri $manualUpdateUrl1 -OutFile $($manualUpdate1)}
-        Add-EsxSoftwareDepot $manualUpdate1
-        Start-Sleep 2
-        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
-    }
-    
-    
-    "2" {
-        echo ""
-        echo "Downloading vmw-depot-index.xml & building ESXi depot, please be patient..."
-        # Retrieve available image profiles from VMware
-        Add-EsxSoftwareDepot https://dl.broadcom.com/$TOKEN/PROD/COMP/ESX_HOST/main/vmw-depot-index.xml
-        Start-Sleep 2
-        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
-        echo ""
-    }
-}
+echo ""
+echo "Downloading $($manualUpdate1) & creating ESXi depot"
+if (!(Test-Path $manualUpdate1)){Invoke-WebRequest -Uri $manualUpdateUrl1 -OutFile $($manualUpdate1)}
+Add-EsxSoftwareDepot $manualUpdate1
+Start-Sleep 2
+$imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
 
 # Print a list of available profiles to choose from
 for ($i = 0; $i -lt $imageProfiles.Count; $i++) {
@@ -80,20 +54,22 @@ echo ""
 echo "Finished retrieving $imageProfile"
 echo ""
 
-if (!(Test-Path $usbFling)){Invoke-WebRequest -Method "GET" $flingUrl$($usbFling) -OutFile $($usbFling)}
+if (!(Test-Path $usbNicFling)){Invoke-WebRequest -Method "GET" $flingUrl$($usbNicFling) -OutFile $($usbNicFling)}
+if (!(Test-Path $realtekNicFling)){Invoke-WebRequest -Method "GET" $flingUrl$($realtekNicFling) -OutFile $($realtekNicFling)}
 
 echo ""
 echo "Adding extra packages to the local depot"
 echo ""
 
 Add-EsxSoftwareDepot "$($imageProfile).zip"
-Add-EsxSoftwareDepot $usbFling
+Add-EsxSoftwareDepot $usbNicFling
+Add-EsxSoftwareDepot $realtekNicFling
 
 echo ""
 echo "Creating a custom profile" 
 echo ""
 
-$newProfileName = $($imageProfile.Replace("standard", "usbnic"))
+$newProfileName = $($imageProfile.Replace("standard", "usbnic-realteknic"))
 $newProfile = New-EsxImageProfile -CloneProfile $imageProfile -name $newProfileName -Vendor "Itiligent"
 Set-EsxImageProfile -ImageProfile $newProfile -AcceptanceLevel CommunitySupported
 
@@ -102,6 +78,7 @@ echo "Injecting extra packages into the custom profile"
 echo ""
 
 Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "vmkusb-nic-fling" -Force
+Add-EsxSoftwarePackage -ImageProfile $newProfile -SoftwarePackage "if-re" -Force
 
 echo ""
 echo "Exporting the custom profile to an ISO..."
@@ -110,6 +87,8 @@ echo ""
 Export-ESXImageProfile -ImageProfile $newProfile -ExportToIso -filepath "$newProfileName.iso" -Force
 Get-EsxSoftwareDepot | Remove-EsxSoftwareDepot
 
+$isoPath = "$newProfileName.iso"
 echo ""
 echo "Build complete!"
+echo "Created ISO: $isoPath"
 echo ""
